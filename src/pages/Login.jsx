aba1a23b759257c8403985013d02a8e873e26e84
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2025-06-14 23:59:15 +0200

    feat(auth): set up basic login and register functionality using Noroff API
    Implemented base authentication system using Zustand for state management
    and react-hook-form for handling forms. Connected login and register
    forms to the official Noroff API and tested functionality successfully.
    Includes storing user and token in Zustand, basic console feedback and navigation after login/ register.
    
    No styling applied.
    Fixed layout.jsx to push footer down to bottom.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,38 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { login } from "../api/auth";
+import { useAuthStore } from "../store/authStore";
+
 export default function Login() {
-  return <div>Login </div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    try {
+      const result = await login(data);
+      useAuthStore.getState().login(result.data, result.token);
+      console.log(" Login completed");
+      navigate("/"); // home
+    } catch (error) {
+      console.log(" Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>Login</h2>
+      <input placeholder="Email" {...register("email", { required: true })} />
+      <input
+        placeholder="Password"
+        type="password"
+        {...register("password", { required: true })}
+      />
+      <button type="submit">Log in</button>
+    </form>
+  );
 }
